1ba9d04efb9d7b1889b113958fa7fc9a70fab966
4kalol <yuya.shikata.work@gmail.com>
2023-08-01 00:38:00 +0900

    マルチログイン対応

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,6 +6,10 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    // Users
+    protected $user_route = 'user.login';
+    // Admins
+    protected $admin_route = 'admin.login';
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
